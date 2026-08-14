dd2d22be5af41ea47708208b73f3189dd78b6b59
amy312james <amy312james@outlook.com>
2026-08-14 12:14:52 +0530

    Create Receipt.js

---
+++ src/components/Receipt.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Receipt.css';
+
+function Receipt() {
+  return (
+    <div className="receipt">
+      <h2>Receipt</h2>
+      <p>Thank you for your payment.</p>
+    </div>
+  );
+}
+
+export default Receipt;
